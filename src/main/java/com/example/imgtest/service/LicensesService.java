765c2d93f5858cfe098b49fa9316f42e2e284476
Giuseppe Calabrese <giuseppe.calabrese@metapack.com>
2021-07-21 23:27:15 +0100

    service layer TDD: tests succeed

--- src/main/java/com/example/imgtest/service/LicensesService.java
+++ src/main/java/com/example/imgtest/service/LicensesService.java
@@ -1,9 +1,14 @@
 package com.example.imgtest.service;
 
 import com.example.imgtest.repository.LicensesRepository;
+import com.example.imgtest.repository.config.LicenseRecord;
+import com.example.imgtest.repository.config.LicenseType;
+import java.util.List;
+import java.util.UUID;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
 
 @Log4j2
 @Service
@@ -13,6 +18,10 @@ public class LicensesService {
 
     public LicensesService(@Autowired  LicensesRepository licensesRepository) {
         this.licensesRepository = licensesRepository;
+    }
+
+    Mono<List<LicenseRecord>> getLicenses(UUID customerId, LicenseType type) {
+        return licensesRepository.getLicenses(customerId, type);
     }
 
 }
